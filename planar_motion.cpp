b12257de7159b9598b37bc48bf2a54301f65a128
Tom Bennett <thomas.bennett5@gmail.com>
2020-08-04 16:21:07 +0100

    completed more of planar motion program

--- planar_motion.cpp
+++ planar_motion.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 #include <iostream>
 #include <string>
 
@@ -8,7 +8,8 @@ int main(int argc, char const *argv[])
 {
     double x0, y0, R, x,y,vx,vy,t,t0,t_final,dt;
     double theta, omega;
-    string buf;
+    double PI = 2*acos(0.0);
+    string buf, buf2, buf3;
 
     cout << "# Enter omega: \n";
     cin >> omega;
@@ -27,7 +28,16 @@ int main(int argc, char const *argv[])
     cout << "# x0    = " << x0 << " y0= " << y0
             << " R = " << R << endl;
     
-    cout << "# t 0= " << t0 << " t f = " << t_final << " dt= " << dt << endl;
+    cout << "# t0= " << t0 << " tf = " << t_final << " dt= " << dt << endl;
+
+    if ( R <=0.0) { cerr << "Illegal value R    \n";
+            exit(1);}
+    if(omega<=0.0){cerr << "Illegal value i    \n"; exit(1);}
+            exit(1);
+    cout  << "# T = " << 20*PI/omega;
+
+
+
 
 
 
